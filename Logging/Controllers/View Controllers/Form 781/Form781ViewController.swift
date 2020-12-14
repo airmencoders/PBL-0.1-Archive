55c1f9417d25b3e369481c07d2eb85c05e016d5a
Pete Misik <pmisik@revacomm.com>
2020-12-14 17:30:39 -0500

    Fixed the commented out PDF send.  Form will flow through people's email.

--- Logging/Controllers/View Controllers/Form 781/Form781ViewController.swift
+++ Logging/Controllers/View Controllers/Form 781/Form781ViewController.swift
@@ -170,7 +170,7 @@ struct FlightListActivityController {
         let objectsToShare = [
             title,
             formattedMessage,
-//            pdfDoc.dataRepresentation()!
+            pdfDoc.dataRepresentation()!
         ] as [Any]
         
         let avc = UIActivityViewController(activityItems: objectsToShare, applicationActivities: nil)
