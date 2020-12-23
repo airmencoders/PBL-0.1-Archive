ca102532fa7360b21dcd58e8c7f330aae3d29911
Pete Hoch <phoch.ctr@revacomm.com>
2020-12-22 19:40:14 -1000

    Remove access to the non existant scroll view.

--- Logging/Controllers/View Controllers/Form 781/Form781ViewController.swift
+++ Logging/Controllers/View Controllers/Form 781/Form781ViewController.swift
@@ -58,33 +58,12 @@ class Form781ViewController: UIViewController {
     // MARK: - Methods
     
     func setUpViews() {
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name:UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name:UIResponder.keyboardWillHideNotification, object: nil)
         guard let form = Form781Controller.shared.getCurrentForm() else {
             return
         }
         missionDateLabel.text = "MISSION \(form.date)"
     }
     
-    @objc func keyboardWillShow(notification:NSNotification) {
-
-        guard let userInfo = notification.userInfo else {
-            return
-        }
-        var keyboardFrame: CGRect = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
-        keyboardFrame = self.view.convert(keyboardFrame, from: nil)
-
-        var contentInset: UIEdgeInsets = self.scrollView.contentInset
-        contentInset.bottom = keyboardFrame.size.height + 20
-        scrollView.contentInset = contentInset
-    }
-
-    @objc func keyboardWillHide(notification:NSNotification) {
-
-        let contentInset: UIEdgeInsets = UIEdgeInsets.zero
-        scrollView.contentInset = contentInset
-    }
-
     // MARK: - Actions
     
     @IBAction func sendButtonTapped(_ sender: UIButton) {
